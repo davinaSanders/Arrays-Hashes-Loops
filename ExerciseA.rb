654485c518592386743a0cf7befde2ece7e62561
davina <davina.kuhnel@gmail.com>
2018-04-12 09:43:45 +0100

    final commit

--- ExerciseA.rb
+++ ExerciseA.rb
@@ -3,13 +3,14 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 p stops.push("Edinburgh Waverley")
 p stops.unshift("Glasgow Queen St")
 p stops.insert(4,"Polmont")
-p stops[5]
+p stops[5] #stops.index("name of item") or you can do stops.find_index("name of item")
 stops.delete("Livingston")
 p stops
-stops.delete(stops[2])
+stops.delete(stops[2]) #stops.delete_at(index number)
 p stops
-p stops.length
-p stops.reverse
+p stops.length #stops.count or stops.size
+p stops.at()
+p stops.fetch()
 p stops.reverse
 for stop in stops
   p stop
